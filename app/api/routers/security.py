a2f99a202c4b5c295e357097a49de0b2233f1638
DaPandaOfficial <miroslav.rosenov39@gmail.com>
2026-05-31 22:26:50 +0300

    feat: Moving to cookie based storage for JWT and Refresh
    Since the frontend would most likely be based on Jinja2 templates, it makes most sense to keep the authentication in HTTP only SECURE cookies

--- app/api/routers/security.py
+++ app/api/routers/security.py
@@ -4,24 +4,25 @@ import datetime
 import secrets
 import jwt
 
+from hashlib import sha256
 from pwdlib.exceptions import UnknownHashError
 from sqlalchemy.exc import IntegrityError
 from loguru import logger as log
 from typing import Annotated, Dict, Optional
 from pwdlib import PasswordHash
 
-from fastapi import HTTPException, Request, status, Depends
+from fastapi import HTTPException, Request, Response, status, Depends
 from fastapi.routing import APIRouter
 
 from app.config import Config
-from app.models.database import DatabaseClient, RefreshToken, SessionDependency, User
-from app.models.requests import RefreshTokenRequest, UserAuthRequest, Token
+from app.models.database import RefreshToken, SessionDependency, User
+from app.models.requests import UserAuthRequest
 
 auth_router = APIRouter(prefix="/authentication")
 password_hash = PasswordHash.recommended()
 
 
-def verify_hash(value: str, hashed_value: str) -> bool:
+def verify_password(value: str, hashed_value: str) -> bool:
     try:
         return password_hash.verify(value, hashed_value)
     except UnknownHashError as exc:
@@ -31,8 +32,18 @@ def verify_hash(value: str, hashed_value: str) -> bool:
         return False
 
 
+def hash_password(value: str) -> str:
+    try:
+        return password_hash.hash(value)
+    except Exception as exc:
+        log.opt(exception=exc).error(
+            "Failed to hash value ('{}' with '{}') - {}", value, exc
+        )
+        raise exc
+
+
 def hash_value(value: str) -> str:
-    return password_hash.hash(value)
+    return sha256(value.encode("utf-8")).hexdigest()
 
 
 def generate_refresh_token() -> str:
@@ -62,10 +73,10 @@ def decode_jwt_token(token: str) -> Optional[Dict]:
         return payload
 
     except jwt.ExpiredSignatureError as exc:
-        log.warning("Failed to decode JWT token ({}) - authorization expired", token)
+        log.warning("Failed to decode JWT token ({}) - authentication expired", token)
         raise exc
 
-    except jwt.InvalidSignatureError:
+    except jwt.InvalidSignatureError as exc:
         log.warning("Failed to decode JWT token ({}) - invalid signature", token)
         raise exc
 
@@ -76,81 +87,148 @@ def decode_jwt_token(token: str) -> Optional[Dict]:
         raise exc
 
 
-async def get_authentication_header(request: Request) -> str:
-    if not (authentication_header := request.headers.get("Authorization")):
+async def get_access_token(
+    request: Request,
+) -> str:  # We are going to use HTTP & Secure cookies for storing our access and refresh tokens since the applocation would use Jinja2 for frontend instead of external app
+    if not (access_token := request.cookies.get("access_token")):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
-    return authentication_header.lstrip("Bearer").strip()
+    return access_token
+
+
+async def get_refresh_token(request: Request) -> Optional[str]:
+    return request.cookies.get("refresh_token")
 
 
 async def get_signed_user(
-    session: SessionDependency, auth_header: AuthDependency
+    session: SessionDependency,
+    access_token: AccessTokenDependency,
+    refresh_token: RefreshTokenDependency,
+    http_response: Response,
 ) -> Optional[User]:
     try:
-        payload = decode_jwt_token(auth_header)
-        return await User.fetch_user(session, payload["email"])
+        payload = decode_jwt_token(access_token)
+        return await User.get_by_email(session, payload["sub"])
 
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization expired"
-        )
+    except jwt.ExpiredSignatureError:  # Here we'll do silent retry to refresh the access token with the provided refresh token and rorate the refresh
+        if (
+            refresh_token is not None
+            and (
+                stored_refresh_token := await RefreshToken.get(
+                    session, hash_value(refresh_token)
+                )
+            )
+            is not None
+        ):
+            user = await User.get_by_id(session, stored_refresh_token.owner_id)
+            access_token_lifespan = datetime.timedelta(minutes=15)
+            access_token = encode_jwt_token(
+                payload={"sub": user.email}, lifespan=access_token_lifespan
+            )
+
+            refresh_token_lifespan = datetime.timedelta(days=31)
+            refresh_token = generate_refresh_token()
+
+            log.warning(
+                "Automatically refreshing JWT access token for user ({}) and rotating credentials",
+                stored_refresh_token.owner_id,
+            )
+            await RefreshToken.update_refresh_token(
+                session,
+                hash_value(stored_refresh_token.refresh_token),
+                hash_value(refresh_token),
+            )
+
+            http_response.set_cookie(
+                key="refresh_token",
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite="lax",
+                max_age=int(refresh_token_lifespan.total_seconds()),
+            )
+
+            http_response.set_cookie(
+                key="access_token",
+                value=access_token,
+                httponly=True,
+                secure=True,
+                samesite="lax",
+                max_age=None,  # We need to ensure that it's present to be able to tell which users are with expired JWT and without JWT at all
+            )
+
+            return await User.get_by_id(session, stored_refresh_token.owner_id)
+
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
     except jwt.InvalidSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid authorization"
-        )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
     except Exception as exc:
         log.opt(exception=exc).error("Failed to authenticate user - {}", exc)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
 
-@auth_router.post("/token")
-async def create_token(session: SessionDependency, request: UserAuthRequest) -> None:
-    if not (user := await User.fetch_user(session, request.email)) or not verify_hash(
-        request.password, user.hashed_password
-    ):
+@auth_router.post("/login", status_code=status.HTTP_204_NO_CONTENT)
+async def create_token(
+    session: SessionDependency, request: UserAuthRequest, http_response: Response
+) -> None:
+    if not (
+        user := await User.get_by_email(session, request.email)
+    ) or not verify_password(request.password, user.hashed_password):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Invalid credentials")
 
     if not user.active:
         raise HTTPException(
-            status.HTTP_401_UNAUTHORIZED, "The user account is not active"
+            status.HTTP_400_BAD_REQUEST, "The user account is not active"
         )
 
     if await RefreshToken.is_owner_at_limit(session, user.id):
-        raise HTTPException(
-            status.HTTP_400_BAD_REQUEST, "Active sessions limit reached"
-        )
+        await RefreshToken.drop_last_refresh_token(session, user.id)
 
+    access_token_lifespan = datetime.timedelta(minutes=15)
     access_token = encode_jwt_token(
-        payload={"email": request.email}, lifespan=datetime.timedelta(minutes=15)
+        payload={"sub": user.email}, lifespan=access_token_lifespan
     )
+
+    refresh_token_lifespan = datetime.timedelta(days=31)
     refresh_token = generate_refresh_token()
+
     await RefreshToken.insert(
         session,
         user.id,
         hash_value(refresh_token),
-        datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=31),
+        datetime.datetime.now(tz=datetime.timezone.utc) + refresh_token_lifespan,
     )
 
-    return Token(access_token=access_token, refresh_token=refresh_token)
+    http_response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age=int(refresh_token_lifespan.total_seconds()),
+    )
 
-
-@auth_router.post("/refresh")
-async def refresh_token(
-    session: SessionDependency, request: RefreshTokenRequest
-) -> None: ...
+    http_response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age=None,  # We need to ensure that it's present to be able to tell which users are with expired JWT and without JWT at all
+    )
 
 
 @auth_router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(session: SessionDependency, request: UserAuthRequest) -> None:
     try:
-        await User.insert(session, request.email, request.password)
+        await User.insert(session, request.email, hash_password(request.password))
     except IntegrityError:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail="Email already in use"
-        )
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT)
 
 
-AuthDependency = Annotated[str, Depends(get_authentication_header)]
+AccessTokenDependency = Annotated[str, Depends(get_access_token)]
+RefreshTokenDependency = Annotated[Optional[str], Depends(get_refresh_token)]
 UserDependency = Annotated[User, Depends(get_signed_user)]
--- app/models/database.py
+++ app/models/database.py
@@ -1,8 +1,19 @@
 import datetime
+
 from typing import Annotated, Optional
 
 from fastapi import Depends
-from sqlalchemy import func, select, insert, update, Integer, Text, Boolean, DateTime
+from sqlalchemy import (
+    func,
+    select,
+    insert,
+    update,
+    delete,
+    Integer,
+    Text,
+    Boolean,
+    DateTime,
+)
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
@@ -26,21 +37,20 @@ class Url(Base):
     async def insert(
         session: AsyncSession, url: str, alias: str, created_by: int
     ) -> None:
-        statement = insert(Url).values(origin=url, alias=alias, created_by=created_by)
-        await session.execute(statement)
-        await session.commit()
+        query = insert(Url).values(origin=url, alias=alias, created_by=created_by)
+        await session.execute(query)
 
     @staticmethod
     async def get_url_row(session: AsyncSession, url: str):
-        statement = select(Url).where(lower(Url.origin) == lower(url))
-        result = await session.execute(statement)
+        query = select(Url).where(lower(Url.origin) == lower(url))
+        result = await session.execute(query)
 
         return result.scalar_one_or_none()
 
     @staticmethod
     async def get_url_from_alias(session: AsyncSession, alias: str):
-        statement = select(Url).where(lower(Url.alias) == lower(alias))
-        result = await session.execute(statement)
+        query = select(Url).where(lower(Url.alias) == lower(alias))
+        result = await session.execute(query)
 
         return result.scalar_one_or_none()
 
@@ -49,27 +59,29 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(unique=True, index=True)
+    email: Mapped[str] = mapped_column(Text, unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(Text)
     active: Mapped[bool] = mapped_column(Boolean)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
 
     @staticmethod
-    async def fetch_user(session: AsyncSession, email: str) -> Optional["User"]:
-        statement = select(User).where(lower(User.email) == lower(email))
-        result = await session.execute(statement)
+    async def get_by_email(session: AsyncSession, email: str) -> Optional["User"]:
+        query = select(User).where(lower(User.email) == lower(email))
+        result = await session.execute(query)
+
+        return result.scalar_one_or_none()
+
+    @staticmethod
+    async def get_by_id(session: AsyncSession, id: int) -> Optional["User"]:
+        query = select(User).where(User.id == id)
+        result = await session.execute(query)
 
         return result.scalar_one_or_none()
 
     @staticmethod
     async def insert(session: AsyncSession, email: str, password: str) -> None:
-        from app.api.routers.security import hash_value  # Circular import issue
-
-        statement = insert(User).values(
-            email=email, hashed_password=hash_value(password)
-        )
-        await session.execute(statement)
-        await session.commit()
+        query = insert(User).values(email=email, hashed_password=password)
+        await session.execute(query)
 
 
 class RefreshToken(Base):
@@ -88,31 +100,54 @@ class RefreshToken(Base):
         refresh_token: str,
         expires_at: datetime.datetime,
     ) -> None:
-        statement = insert(RefreshToken).values(
+        query = insert(RefreshToken).values(
             owner_id=owner_id, refresh_token=refresh_token, expires_at=expires_at
         )
-        await session.execute(statement)
-        await session.commit()
+        await session.execute(query)
 
     @staticmethod
     async def update_refresh_token(
         session: AsyncSession, old_value: str, new_value: str
     ) -> None:
-        statement = (
+        query = (
             update(RefreshToken)
             .where(RefreshToken.refresh_token == old_value)
             .values(refresh_token=new_value)
         )
-        await session.execute(statement)
-        await session.commit()
+        await session.execute(query)
 
     @staticmethod
     async def is_owner_at_limit(session: AsyncSession, owner_id: int) -> bool:
-        statement = select(RefreshToken).where(RefreshToken.owner_id == owner_id)
-        result = await session.execute(statement)
+        query = select(RefreshToken).where(RefreshToken.owner_id == owner_id)
+        result = await session.execute(query)
         rows = result.all()
 
         return len(rows) >= Config.MAX_SESIONS_PER_USER
+
+    @staticmethod
+    async def get(
+        session: AsyncSession, refresh_token: str
+    ) -> Optional["RefreshToken"]:
+        query = (
+            select(RefreshToken)
+            .join(User, RefreshToken.owner_id == User.id, full=True)
+            .where(RefreshToken.refresh_token == refresh_token)
+        )
+        result = await session.execute(query)
+
+        return result.scalar_one_or_none()
+
+    @staticmethod
+    async def drop_last_refresh_token(session: AsyncSession, owner_id: int) -> None:
+        sub_query = (
+            select(RefreshToken)
+            .where(RefreshToken.owner_id == owner_id)
+            .order_by(RefreshToken.created_at.asc())
+            .limit(1)
+        )
+        query = delete(RefreshToken).where(RefreshToken.id == sub_query.c.id)
+
+        await session.execute(query)
 
 
 class DatabaseClient:
@@ -146,6 +181,7 @@ class DatabaseClient:
             async with session.begin():
                 try:
                     yield session
+                    await session.commit()
                 except Exception:
                     await session.rollback()
                     raise
